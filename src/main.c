10eb07f493381c30d40963160482e4544f160cd6
Karl <karlding@users.noreply.github.com>
2016-03-19 16:48:15 -0400

    Update code to use ADC driver

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include "drivers/afe.h"
 #include "drivers/gpio_map.h"
-
+#include "drivers/adc12.h"
 
 int main() {
-  struct SPIConfig spi = {
+  const struct SPIConfig spi = {
     .data_out = { GPIO_PORT_P5, GPIO_PIN6 },
     .data_in = { GPIO_PORT_P5, GPIO_PIN7 },
     .clock_out = { GPIO_PORT_P3, GPIO_PIN6 },
@@ -21,11 +21,30 @@ int main() {
     .temp = { -20, 60 }
   };
 
+  static const struct ADC12Config adc12 = {
+    .buffers = {
+      [ADC12_MEM0] = {
+        .pin = { GPIO_PORT_P6, GPIO_PIN0 }, // 5V rail
+        .source = ADC12INCH_0
+      },
+      [ADC12_MEM1] = {
+        .pin = { GPIO_PORT_P6, GPIO_PIN1 }, // 3.3V rail
+        .source = ADC12INCH_1
+      }
+    },
+    .count = 2,
+    .continuous = true
+  };
+
   struct IOMap heartbeat = { GPIO_PORT_P8, GPIO_PIN0 };
-  struct Threshold rail_thresh_05V = { 4.7, 5.3 };
-  struct Threshold rail_thresh_33V = { 3.0, 3.6 };
+  struct Threshold rail_thresh_05V = { 4.7, 5.3 }; // 4.7V -> something, 5.3V -> something
+  struct Threshold rail_thresh_33V = { 3.0, 3.6 }; // 3.0V -> something, 3.6V -> something
+
+  _enable_interrupt();
 
   afe_init(&afe);
+  io_set_state(&heartbeat, IO_HIGH);
+  adc12_init(&adc12);
 
   while (true) {
     uint16_t device;
@@ -33,29 +52,15 @@ int main() {
 
     for (device = 0; device < afe.devices; ++device) {
       // todo: check current and determine whether charging or discharging
-
-
-      union {
-        struct {
-          // d0 and d1 are reserved bits
-          const bool d0;
-          const bool d1;
-          bool cb1 : 1;
-          bool cb2 : 1;
-          bool cb3 : 1;
-          bool cb4 : 1;
-          bool cb5 : 1;
-          bool cb6 : 1;
-        };
-        uint8_t cells_to_balance;
-      } balance;
+      int overvoltage, undervoltage;
+      uint8_t cells_to_balance = 0;
 
       for (input = 0; input < 6; ++input) {
         // cell string voltage
         uint16_t cell_voltage = afe_voltage_conversion(&afe, device, input);
 
-        if (cell_voltage > 1 || cell_voltage < 1) {
-          io_set_state(&heartbeat, IO_HIGH);
+        if (cell_voltage > overvoltage || cell_voltage < undervoltage) {
+          io_set_state(&heartbeat, IO_LOW);
         }
 
         // we want to limit voltages based on a logarithmic threshold
@@ -63,50 +68,41 @@ int main() {
         // [4, 4.15] -> limit to 4.15
         // [4.15, 4.2] -> limit to 4.2
 
-        // use cases to avoid bitshifting magic
-        switch (input) {
-          case 0:
-            balance.cb1 = true;
-            break;
-          case 1:
-            balance.cb2 = true;
-            break;
-          case 2:
-            balance.cb3 = true;
-            break;
-          case 3:
-            balance.cb4 = true;
-            break;
-          case 4:
-            balance.cb5 = true;
-            break;
-          case 5:
-            balance.cb6 = true;
-            break;
-        }
+        // balance string uses flags with bits corresponding to the battery
+        // d0 and d1 are reserved, so bit 2 + cell gives us the correct bit
+        //if (cell_voltage > ...) {
+        //  cells_to_balance |= 1 << (2 + input);
+        //}
 
         // check temperature (aux adc) is within [-20, 60]
         uint16_t cell_temp = afe_aux_conversion(&afe, device, input);
         if (cell_temp > afe.temp.high || cell_temp < afe.temp.low) {
-          io_set_state(&heartbeat, IO_HIGH);
+          io_set_state(&heartbeat, IO_LOW);
         }
       }
 
       // check voltages on 5V and 3.3V rails
-      uint16_t rail_05V = (ADC12MEM0 * 6.6) / 4095;
-      uint16_t rail_33V = (ADC12MEM1 * 6.6) / 4095;
+      uint16_t rail_05V = adc12_sample(&adc12, ADC12_MEM0);
+      uint16_t rail_33V = adc12_sample(&adc12, ADC12_MEM1);
 
       if ((rail_05V < rail_thresh_05V.low || rail_05V > rail_thresh_05V.high) ||
           (rail_33V < rail_thresh_33V.low || rail_33V > rail_thresh_33V.high)) {
-        io_set_state(&heartbeat, IO_HIGH);
+        io_set_state(&heartbeat, IO_LOW);
       }
 
-      // todo: check current for all cells
-
+      // todo: check current for all cells using PWM value
+      //if(((current > 75) || (current < -58)) && (current > -125) && (current < 125)) {
+      //    io_set_state(&heartbeat, IO_LOW);
+      //}
 
 
       // balance any cells needing balancing
-      afe_set_cbx(&afe, device, balance.cells_to_balance);
+      afe_set_cbx(&afe, device, cells_to_balance);
     }
   }
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+  __interrupt void function(void) {
+
 }
