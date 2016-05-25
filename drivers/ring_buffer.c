7af3a4a74aee3b8e61bea268cdf3b0325837a7e1
ckitagawa <calderk2010@gmail.com>
2016-05-25 15:46:01 -0400

    Further optimizations to the code still not consistent

--- drivers/ring_buffer.c
+++ drivers/ring_buffer.c
@@ -2,15 +2,15 @@
 #include "msp430.h"
 #include <string.h>
 
-void ring_buffer_init(struct RingBuffer *ring_buffer)
+void ring_buffer_init(volatile struct RingBuffer *ring_buffer)
 {
-  memset(ring_buffer, 0, sizeof(*ring_buffer));
-  ring_buffer->buffer_end = ring_buffer->buffer + BUFFER_SIZE;
+  memset((void *) ring_buffer, 0, sizeof(*ring_buffer));
+  ring_buffer->buffer_end = (uint16_t*) ring_buffer->buffer + BUFFER_SIZE;
   ring_buffer->head = ring_buffer->buffer;
   ring_buffer->inserts = 0;
 };
 
-void ring_buffer_push(struct RingBuffer *ring_buffer, uint16_t data)
+void ring_buffer_push(volatile struct RingBuffer *ring_buffer, uint16_t data)
 {
   *ring_buffer->head = data;
   ring_buffer->head++;
@@ -21,9 +21,9 @@ void ring_buffer_push(struct RingBuffer *ring_buffer, uint16_t data)
   }
 }
 
-void ring_buffer_read(struct RingBuffer *ring_buffer, uint16_t *return_buffer)
+void ring_buffer_read(volatile struct RingBuffer *ring_buffer, uint16_t *return_buffer)
 {
-  uint16_t *tmp_tail = ring_buffer->head;
+  volatile uint16_t *tmp_tail = ring_buffer->head;
   uint16_t i;
 
   for (i = 0; i < BUFFER_SIZE; i++, tmp_tail++) {
--- drivers/ring_buffer.h
+++ drivers/ring_buffer.h
@@ -6,16 +6,16 @@
 #define BUFFER_SIZE 16
 
 struct RingBuffer {
-  uint16_t buffer[BUFFER_SIZE];
-  uint16_t *buffer_end;
-  uint16_t *head;
-  uint16_t inserts;
+  volatile uint16_t buffer[BUFFER_SIZE];
+  volatile uint16_t *buffer_end;
+  volatile uint16_t *head;
+  volatile uint16_t inserts;
 };
 
-void ring_buffer_init(struct RingBuffer *ring_buffer);
+void ring_buffer_init(volatile struct RingBuffer *ring_buffer);
 
 //Adds next value at the head of the ring buffer
-void ring_buffer_push(struct RingBuffer *ring_buffer, uint16_t data);
+void ring_buffer_push(volatile struct RingBuffer *ring_buffer, uint16_t data);
 
 //Reads from the ring buffer starting at the tail
-void ring_buffer_read(struct RingBuffer *ring_buffer, uint16_t *return_buffer);
+void ring_buffer_read(volatile struct RingBuffer *ring_buffer, uint16_t *return_buffer);
