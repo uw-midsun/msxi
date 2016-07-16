54aa52929250105608abfead24f0e6c9d70c228c
Karl <karlding@users.noreply.github.com>
2016-07-16 12:12:03 -0400

    Fix PWM initialization

--- drivers/pwm.c
+++ drivers/pwm.c
@@ -7,30 +7,27 @@ volatile struct RingBuffer rising = {0};
 
 void pwm_init(const struct PWMConfig *pwm)
 {
+  io_set_peripheral_dir(&pwm->pin_rising, PIN_IN);
+  io_set_peripheral_dir(&pwm->pin_falling, PIN_IN);
   ring_buffer_init(&falling);
   ring_buffer_init(&rising);
-
-  io_set_dir(&pwm->pin_rising, PIN_IN);
-  io_set_dir(&pwm->pin_falling, PIN_IN);
 
   // Second line of the next few blocks are for launchpad
 
   // Timer B capture to CCR0 on rising max priority
-
   TB0CCTL0 = CCIE | CAP | CM_1 | SCS | CCIS_0;
-  //TA0CCTL1 = CCIE | CAP | CM_1 | SCS | CCIS_0;
+//  TA0CCTL1 = CCIE | CAP | CM_1 | SCS | CCIS_0;
 
   // Timer B capture to CCR1 on falling 2nd highest priority
-  TB0CCTL1 = CCIE | CAP | CM_2 | SCS | CCIS_0;
-  //TA0CCTL2 = CCIE | CAP | CM_2 | SCS | CCIS_0;
+  TB0CCTL1 = CCIE | CAP | CM_2 | SCS | CCIS_1;
+//  TA0CCTL2 = CCIE | CAP | CM_2 | SCS | CCIS_0;
 
   // Start the clock use 8 divisions to improve reliability
   TB0CTL = TBSSEL_2 | MC_2 | TBCLR | ID_3;
-  //TA0CTL = TASSEL_2 | MC_2 | TACLR | ID_3;
+//  TA0CTL = TASSEL_2 | MC_2 | TACLR | ID_3;
 
-  __enable_interrupt();
+//  __enable_interrupt();
 
-  __bis_SR_register(GIE);
 }
 
 uint16_t pwm_calculate_duty_cycle(void)
@@ -62,26 +59,32 @@ uint16_t pwm_calculate_duty_cycle(void)
   return duty_cycle;
 }
 
-/*
+
 // ISR Timer on launchpad
-#pragma vector=TIMER0_A1_VECTOR
-__interrupt void TIMER0_A1_ISR(void) {
-  if (TA0IV == 0x02) {
-    ring_buffer_push(&rising, TA0CCR1);
-  } else {
-    ring_buffer_push(&falling, TA0CCR2);
-  }
-}
-*/
+//#pragma vector=TIMER0_A1_VECTOR
+//__interrupt void TIMER0_A1_ISR(void) {
+//  if (TA0IV == 0x02) {
+//    ring_buffer_push(&rising, TA0CCR1);
+//  } else {
+//    ring_buffer_push(&falling, TA0CCR2);
+//  }
+//  TA0CTL &= ~TAIFG;
+//  TA0IV = 0;
+//}
+
 // ISR for Timer B on Plutus
+
 
 #pragma vector=TIMER0_B0_VECTOR
 __interrupt void TB0CCR0_ISR(void) {
   ring_buffer_push(&rising, TB0CCR0);
+  TB0CTL &= ~TBIFG; // clear overflow flag
+  TB0IV = 0;
 }
 
 #pragma vector=TIMER0_B1_VECTOR
 __interrupt void TB0CCR1_ISR(void) {
   ring_buffer_push(&falling, TB0CCR1);
+  TB0CTL &= ~TBIFG;
+  TB0IV = 0;
 }
-
