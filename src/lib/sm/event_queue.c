652edd223be872b8507b29311509932f1c9a5711
Titus Chow <tituschow96@gmail.com>
2016-10-02 01:13:45 -0400

    Converted submodules into flat directories

---
+++ src/lib/sm/event_queue.c
@@ -0,0 +1,95 @@
+#include "event_queue.h"
+#include "msp430.h"
+#include <assert.h>
+
+// The event queue is a FIFO linked list.
+// It uses a pool of event nodes to hold events with a constant memory cost.
+// Note that the pool size is a hand-tuned variable. It is currently a wild guess.
+
+// TODO: Add check for empty queue to allow sleeping/waking to conserve power?
+
+#define EVENT_POOL_SIZE 10
+
+struct EventNode {
+  struct Event e;
+  struct EventNode *next;
+};
+
+struct EventQueue {
+  struct EventNode *first;
+  struct EventNode *last;
+};
+
+static struct EventNode node_pool[EVENT_POOL_SIZE];
+static struct EventQueue event_queue;
+
+void event_queue_init(void) {
+  struct EventNode *temp_node;
+  for (temp_node = node_pool; temp_node < node_pool + EVENT_POOL_SIZE; temp_node++) {
+    temp_node->e = NULL_EVENT;
+    temp_node->next = NULL;
+  }
+  event_queue.first = QUEUE_EMPTY;
+  event_queue.last = QUEUE_EMPTY;
+}
+
+// Returns a pointer to the next free node in the pool.
+static struct EventNode *prv_get_free_node(void) {
+  struct EventNode *temp_node;
+  for (temp_node = node_pool; temp_node < node_pool + EVENT_POOL_SIZE; temp_node++) {
+    if (temp_node->e.id == NULL_EVENT_ID) {
+      return temp_node;
+    }
+  }
+  // Should never reach here - increase pool if it does.
+  assert(0 && "Event pool is too small!");
+  return NULL;
+}
+
+// Nodes with the default values are assumed to be free.
+static void prv_release_node(struct EventNode *node) {
+  node->e = NULL_EVENT;
+  node->next = NULL;
+}
+
+// Sets e as the queue's last node using a node in the pool.
+//  Note that this is unsafe - Interrupts may cause race conditions.
+void event_raise_isr(EventID id, uint64_t data) {
+  struct EventNode *event_node = prv_get_free_node();
+  event_node->e = (struct Event) { id, data };
+  event_node->next = QUEUE_EMPTY;
+
+  if (event_queue.last == QUEUE_EMPTY) {
+    event_queue.first = event_node;
+  } else {
+    event_queue.last->next = event_node;
+  }
+  event_queue.last = event_node;
+}
+
+// This disables interrupts to prevent race conditions.
+void event_raise(EventID id, uint64_t data) {
+  __disable_interrupt();
+  event_raise_isr(id, data);
+  __enable_interrupt();
+}
+
+// Pops the first node. If there aren't any events queued, it just returns an empty node.
+struct Event event_get_next(void) {
+  if (event_queue.first == QUEUE_EMPTY) {
+    return NULL_EVENT;
+  }
+
+  __disable_interrupt();
+  struct EventNode *first_node = event_queue.first;
+  struct Event event = first_node->e;
+  event_queue.first = first_node->next;
+  if (first_node->next == QUEUE_EMPTY) {
+    event_queue.last = QUEUE_EMPTY;
+  }
+
+  prv_release_node(first_node);
+  __enable_interrupt();
+
+  return event;
+}
